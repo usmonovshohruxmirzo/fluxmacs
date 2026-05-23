ee6107b9f5eb81a94db86a0e2cdee49da01da1ae
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-24 00:10:40 +0500

    refactor(init): impove package management and theme setup

--- init.el
+++ init.el
@@ -1,7 +1,44 @@
+;; -------------------------
+;; Package system
+;; -------------------------
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+;; -------------------------
+;; UI
+;; -------------------------
 (global-display-line-numbers-mode 1)
 
-(require 'evil)
+;; -------------------------
+;; Evil (Vim layer)
+;; -------------------------
+(unless (package-installed-p 'evil)
+  (package-refresh-contents)
+  (package-install 'evil))
 
+(require 'evil)
 (evil-mode 1)
 
+;; -------------------------
+;; Theme
+;; -------------------------
+(unless (package-installed-p 'dracula-theme)
+  (package-refresh-contents)
+  (package-install 'dracula-theme))
+
 (load-theme 'dracula t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
