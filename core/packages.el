847fa665d79ca348ec439fe6bb402159cf265f69
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-24 13:42:04 +0500

    feat(packages): add package management setup with MELPA and GNU archives

---
+++ core/packages.el
@@ -0,0 +1,8 @@
+(require 'package)
+
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(provide 'packages)
