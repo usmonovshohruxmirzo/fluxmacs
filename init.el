0606482e87e674fe9f9a10b060cc1619e66a094d
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-24 13:44:36 +0500

    refactor(init): enhance conf by removing package system and theme setup

--- init.el
+++ init.el
@@ -1,44 +1,14 @@
-;; -------------------------
-;; Package system
-;; -------------------------
-(require 'package)
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-        ("gnu" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
+;; Core paths
+(add-to-list 'load-path "~/.config/emacs/core")
+(add-to-list 'load-path "~/.config/emacs/modules/editor")
+(add-to-list 'load-path "~/.config/emacs/modules/ui")
 
-;; -------------------------
+;; Core
+(require 'packages)
+(require 'defaults)
+
+;; Editor
+(require 'flux-evil)
+
 ;; UI
-;; -------------------------
-(global-display-line-numbers-mode 1)
-
-;; -------------------------
-;; Evil (Vim layer)
-;; -------------------------
-(unless (package-installed-p 'evil)
-  (package-refresh-contents)
-  (package-install 'evil))
-
-(require 'evil)
-(evil-mode 1)
-
-;; -------------------------
-;; Theme
-;; -------------------------
-(unless (package-installed-p 'dracula-theme)
-  (package-refresh-contents)
-  (package-install 'dracula-theme))
-
-(load-theme 'dracula t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'flux-theme)
